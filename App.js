3f58bc8b14ecb25e0a46b59e9791762270e57901
청 <riplac0314@naver.com>
2021-01-11 16:00:48 +0900

    #2.3

--- App.js
+++ App.js
@@ -13,29 +13,34 @@ export default class extends React.Component{
     isLoading : true
   }
   getWeather = async(latitude, longitude) => {
-    const {data} = await axios.get(
+    const {data: {main: {temp}, weather}} = await axios.get(
       // ` <- 백틱을 사용한다
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
       );
-      this.setState({isLoading:false, temp: data.main.temp})
-  }
+      this.setState({isLoading:false,
+        confition: weather[0].main,
+        temp: temp});
+  };
   getLocation = async() =>{
     try {
       await Location.requestPermissionsAsync();
       const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
     
-  }
+  };
   componentDidMount(){
     this.getLocation();
   }
-  render(){
-    const {isLoading, temp} = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}></Weather>;
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
 
